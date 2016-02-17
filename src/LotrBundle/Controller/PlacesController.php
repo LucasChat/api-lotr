edab76145fc18eb10223ce9148485f2e43d6e450
Wolfens <asubias78@gmail.com>
2016-02-17 20:53:56 +0100

    getPlacesAction + routing

--- src/LotrBundle/Controller/PlacesController.php
+++ src/LotrBundle/Controller/PlacesController.php
@@ -2,9 +2,19 @@
 
 namespace LotrBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class PlacesController extends Controller
+class PlacesController extends FOSRestController
 {
+    public function getPlacesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $places = $em->getRepository('LotrBundle:Places')->findAll();
+
+        $view = $this->view($places);
+        return $this->handleView($view);
+    }
 }
