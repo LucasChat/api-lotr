2d7851a48e77019eecd9407acf12d1c0492f4ffc
jlyw <jean.lawyimwan@gmail.com>
2016-02-18 14:46:23 +0100

    getEventsAction + routing

--- src/LotrBundle/Controller/EventsController.php
+++ src/LotrBundle/Controller/EventsController.php
@@ -2,9 +2,19 @@
 
 namespace LotrBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class EventsController extends Controller
+class EventsController extends FOSRestController
 {
+	public function getEventsAction()
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$events = $em->getRepository('LotrBundle:Events')->findAll();
+
+		$view = $this->view($events);
+		return $this->handleView($view);
+	}
 }
