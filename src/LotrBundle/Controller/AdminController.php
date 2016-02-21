8238919bd49eb260264912a310c1138fa1db9527
jlyw <jean.lawyimwan@gmail.com>
2016-02-21 15:19:29 +0100

    newEventAction - Affichage du formulaire pour aujouter un événement

--- src/LotrBundle/Controller/AdminController.php
+++ src/LotrBundle/Controller/AdminController.php
@@ -2,9 +2,16 @@
 
 namespace LotrBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use FOS\RestBundle\Controller\FOSRestController;
+use LotrBundle\Entity\Events;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Tests\Extension\Core\Type\IntegerTypeTest;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -24,8 +31,73 @@ class AdminController extends FOSRestController
             'events' => $em->getRepository('LotrBundle:Events')->findAll(),
             'races' => $em->getRepository('LotrBundle:Races')->findAll(),
             'status' => $em->getRepository('LotrBundle:Status')->findAll(),
-            'activePage' => 'dashboard',
+            'pageActive' => 'dashboard',
         ));
     }
 
+    /**
+     * @Route("/new/event")
+     */
+    public function newEventAction(Request $request)
+    {
+        $event = new Events();
+
+        $form = $this->createFormBuilder($event)
+            ->add('slug', TextType::class, array(
+                'label' => 'Slug',
+                'attr' => array(
+                    'placeholder' => 'mon-nouvel-evenement',
+                ),
+            ))
+            ->add('name', TextType::class, array(
+                'label' => 'Nom de l\'événement',
+                'attr' => array(
+                    'placeholder' => 'Mon nouvel événement',
+                ),
+            ))
+            ->add('date', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'label' => 'Date de début',
+            ))
+            ->add('dateEnd', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'label' => 'Date de fin',
+            ))
+            ->add('coordX', IntegerType::class, array(
+                'label' => 'Coordonnée X',
+                'empty_data' => -1,
+                'data' => -1,
+                'attr' => array(
+                    'min' => -1,
+                ),
+            ))
+            ->add('coordY', IntegerType::class, array(
+                'label' => 'Coordonnée Y',
+                'empty_data' => -1,
+                'data' => -1,
+                'attr' => array(
+                    'min' => -1,
+                ),
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Ajouter un événement'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($event);
+            $em->flush();
+
+            return $this->redirectToRoute('event', array('id' => $event->getSlug()));
+        }
+
+        return $this->render('admin/new_event.html.twig', array(
+           'pageActive' => 'new_event',
+            'form' => $form->createView(),
+        ));
+    }
 }
