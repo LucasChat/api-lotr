5896d1a352aa94fabe4d72d7b913ee9496c45247
jlyw <jean.lawyimwan@gmail.com>
2016-02-23 01:11:11 +0100

    CharactersControllerTest - Continue unit test

--- src/LotrBundle/Repository/CharactersTripRepository.php
+++ src/LotrBundle/Repository/CharactersTripRepository.php
@@ -182,7 +182,7 @@ class CharactersTripRepository extends EntityRepository
 
         if(!$result)
         {
-            $result = "error : nobody here at this date";
+            $result = "error : nobody here at this period";
         }
 
         return $result;
